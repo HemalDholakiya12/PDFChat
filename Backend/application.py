1da8be7baa356d2d67a7fc629664994be1610f73
HemalDholakiya12 <hemaldholakiya12@gmail.com>
2025-04-16 17:36:32 +0530

    Update application.py

--- Backend/application.py
+++ Backend/application.py
@@ -10,7 +10,6 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000", 
-        "https://pdfchat-psi.vercel.app/",
     ],
     allow_credentials=True,
     allow_methods=["*"],
